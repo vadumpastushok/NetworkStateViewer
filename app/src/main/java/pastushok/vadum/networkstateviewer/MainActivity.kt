4d187615cd2852279e2e3ceb2d0c6f52c03c6f7c
Vadum <vadumpast@gmail.com>
2021-10-04 23:43:28 +0300

    added functionality

--- app/src/main/java/pastushok/vadum/networkstateviewer/MainActivity.kt
+++ app/src/main/java/pastushok/vadum/networkstateviewer/MainActivity.kt
@@ -1,29 +1,40 @@
     package pastushok.vadum.networkstateviewer
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.net.wifi.WifiManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.format.Formatter
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import pastushok.vadum.networkstateviewer.databinding.ActivityMainBinding
 
     class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var ipAddress = "12"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        binding = ActivityMainBinding.inflate(LayoutInflater.from(applicationContext))
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        setContentView(binding.root)
         getNetworkData()
-        showNetworkData()
     }
 
     private fun getNetworkData(){
-
+        val wifiManager = this.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        ipAddress = Formatter.formatIpAddress(wifiManager.connectionInfo.ipAddress)
+        showNetworkData()
     }
 
     private fun showNetworkData(){
-
+        binding.textView.text = "IpAddress: $ipAddress"
     }
 
 }
